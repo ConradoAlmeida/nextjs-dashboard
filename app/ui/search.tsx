9473717e4506b9adce35db1b3e1d923cde332bff
ConradoAlmeida <conrado.almeidaa@gmail.com>
2024-10-28 15:38:33 -0300

    sistema de paginação

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
     if (term) {
       params.set('query', term);
     } else {
@@ -22,8 +23,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`);
 
   }, 300);
-
-
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
