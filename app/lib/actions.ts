a94ea8c1255d17b750c50717cd6e39195282cd7f
ConradoAlmeida <conrado.almeidaa@gmail.com>
2024-10-28 17:11:26 -0300

    Criação de invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 
 const FormSchema = z.object({
@@ -30,6 +32,10 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+
 
     // console.log(rawFormData)
     // console.log(typeof rawFormData.amount);
